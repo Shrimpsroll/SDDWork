945ea7e007f28fa624c47da1a93004649ac05f23
Shrimpsroll <83622348+Shrimpsroll@users.noreply.github.com>
2023-06-07 14:57:11 +1000

    Update numberThree.cpp

--- LA14_cppCoding/numberThree.cpp
+++ LA14_cppCoding/numberThree.cpp
@@ -1,1 +1,22 @@
+//Write a program that asks the user for a number n and prints the sum 
+//of the numbers 1 to n (check the weekly algorithm theory Q4 for some help ;-) 
+//though you don't need to create a subroutine yet).
+#include <iostream>
+using namespace std;
 
+int main() {
+    int sum = 0;
+    int counter = 1;
+    int totalSum = 0;
+    int userInput;
+
+    cout << "give me a number" << endl;
+    cin >> userInput;
+    while (counter<=userInput) {
+        sum ++;
+        totalSum += sum;
+        counter++;
+    }
+    cout << "total is '" << totalSum << "'" << endl;
+
+}
