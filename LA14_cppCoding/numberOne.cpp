74ac8db0de34f14d8161158b2ca9642e86557adc
Shrimpsroll <83622348+Shrimpsroll@users.noreply.github.com>
2023-06-07 14:00:36 +1000

    Update numberOne.cpp

--- LA14_cppCoding/numberOne.cpp
+++ LA14_cppCoding/numberOne.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+using namespace std;
+
+string name;
+
+int main() {
+  cout << "Hello! What is your name?" << endl;
+  cin >> name;
+  cout << "Hello " << name << " How are you today?";
+}
